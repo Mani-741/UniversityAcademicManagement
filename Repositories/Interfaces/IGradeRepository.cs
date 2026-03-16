2a39c5814fd1e3eeb7ff33a3b78bb4e3b9e033b0
Garg <2475938@cognizant.com>
2026-03-16 16:04:03 +0530

    [Divyansh Garg] UPDATE DONE

--- Repositories/Interfaces/IGradeRepository.cs
+++ Repositories/Interfaces/IGradeRepository.cs
@@ -4,6 +4,7 @@ namespace UniversityAcademicManagementSystem.Repositories.Interfaces
 {
     public interface IGradeRepository
     {
+        Grade GetGradeById(int gid);
         IEnumerable<Grade> GetGradesByCourse(int courseId);
         void SubmitGrade(Grade grade);
         void UpdateGrade(Grade grade);
